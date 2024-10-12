bf34b14cd6ec878b6b0a080a0e87cb12e0aef101
mr3iscuit <eyvaz.bayramov2018@gmail.com>
2024-10-12 21:15:08 +0400

    add ConfigModel configuration settings are based on this model

--- src/main/java/org/exidehelper/ConfigModel.java
+++ src/main/java/org/exidehelper/ConfigModel.java
@@ -1,4 +1,14 @@
 package org.exidehelper;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class ConfigModel {
+    private String homeFolder;
 }
