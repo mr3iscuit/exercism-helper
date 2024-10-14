a93afe90c0324c871743e514b48496eeccc064b8
mr3iscuit <eyvaz.bayramov2018@gmail.com>
2024-10-14 23:41:04 +0400

    exercism "configure" command bindings

--- src/main/java/org/exidehelper/commands/ConfigCommand.java
+++ src/main/java/org/exidehelper/commands/ConfigCommand.java
@@ -1,15 +1,47 @@
 package org.exidehelper.commands;
 
+import org.exidehelper.exercismWrapperService.ConfigCommandFlags;
+import org.exidehelper.exercismWrapperService.GlobalFlags;
 import org.exidehelper.exercismWrapperService.IExercismAPIWrapperService;
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
 
+import java.io.IOException;
+
 @Command(name = "configure", description = "Configuration related operations")
 public class ConfigCommand implements Runnable {
-
     private final IExercismAPIWrapperService exercismAPIWrapperService;
-    @CommandLine.Option(names = {"--workspace", "-w"}, description = "Set the exercism workspace directory")
+
+    // Global Flags
+    @CommandLine.Option(names = {"--timeout"}, description = "override the default HTTP timeout (seconds)")
+    private Integer timeout;
+
+    @CommandLine.Option(names = {"--unmask-token"}, description = "will unmask the API during a request/response dump")
+    private boolean unmaskToken;
+
+    @CommandLine.Option(names = {"--verbose"}, description = "verbose output")
+    private boolean verbose;
+
+
+    // Flags
+    @CommandLine.Option(names = {"--show", "-s"}, description = "show the current configuration")
+    private boolean show;
+
+    @CommandLine.Option(names = {"--help", "-h"}, description = "help for configure")
+    private boolean help;
+
+    @CommandLine.Option(names = {"--no-verify"}, description = "skip online token authorization check")
+    private boolean noVerify;
+
+
+    @CommandLine.Option(names = {"--workspace", "-w"}, description = "directory for exercism workspace")
     private String workspace;
+
+    @CommandLine.Option(names = {"--token", "-t"}, description = "authentication token used to connect to the site")
+    private String token;
+
+    @CommandLine.Option(names = {"--api", "-a"}, description = "API base url")
+    private String api;
 
 
     public ConfigCommand(IExercismAPIWrapperService apiWrapperService) {
@@ -18,8 +50,33 @@ public class ConfigCommand implements Runnable {
 
     @Override
     public void run() {
-        if (workspace != null) {
-            exercismAPIWrapperService.setWorkspace(workspace);
+
+        GlobalFlags globalFlags = new GlobalFlags(
+                timeout,
+                unmaskToken,
+                verbose
+        );
+
+        ConfigCommandFlags configCommandFlags = new ConfigCommandFlags(
+                show,
+                help,
+                noVerify,
+
+                workspace,
+                token,
+                api
+        );
+
+        try {
+            System.out.print(
+                    exercismAPIWrapperService.configure(globalFlags, configCommandFlags)
+            );
+        }
+        catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        catch (InterruptedException e) {
+            throw new RuntimeException(e);
         }
     }
 }
--- src/main/java/org/exidehelper/exercismWrapperService/ExercismAPIWrapperService.java
+++ src/main/java/org/exidehelper/exercismWrapperService/ExercismAPIWrapperService.java
@@ -4,34 +4,79 @@ import org.exidehelper.CommandExecutor;
 import org.exidehelper.appConfig.ConfigService;
 
 import java.io.IOException;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class ExercismAPIWrapperService implements IExercismAPIWrapperService {
 
     ConfigService configService;
+    private ArrayList<String> command;
 
     public ExercismAPIWrapperService(ConfigService configService) {
         this.configService = configService;
+        this.command = new ArrayList<String>(List.of(configService.getExercismExecutable()));
     }
 
     @Override
-    public String getWorkspace() {
-        try {
-            return CommandExecutor.executeCommand(new String[]{configService.getExercismExecutable(), "workspace"});
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
+    public IExercismAPIWrapperService workspace() {
+
+        command.add("workspace");
+        return this;
     }
 
     @Override
-    public String setWorkspace(String path) {
+    public String runCommand() throws IOException, InterruptedException {
+
+        String res;
+
         try {
-            return CommandExecutor.executeCommand(new String[]{configService.getExercismExecutable(), "configure", "--workspace", path});
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
+            res = CommandExecutor.executeCommand(command);
         }
+
+        catch (IOException e) {
+            throw new RuntimeException(
+                    "Could not execute command %s".formatted(
+                        command
+                            .stream()
+                            .collect(
+                                    Collectors.joining(" ")
+                            )
+                    ),
+                    e
+            );
+        }
+
+        command.clear();
+        return res;
+    }
+
+    @Override
+    public String configure(GlobalFlags gf, ConfigCommandFlags cf) throws IOException, InterruptedException {
+        List<String> command = List.of(new String[]{
+                configService.getExercismExecutable(),
+                gf.unmaskToken ? "--unmask-token" : "",
+                gf.verbose ? "--verbose" : "",
+
+                gf.timeout != null ? "--timeout" : "",
+                gf.timeout != null ? String.valueOf(gf.timeout) : "",
+
+                "configure",
+
+                cf.help ? "--help" : "",
+                cf.noVerify ? "--no-verify" : "",
+                cf.show ? "--show" : "",
+
+                cf.api != null ? "--api" : "",
+                cf.api != null ? cf.api : "",
+
+                cf.token != null ? "--token" : "",
+                cf.token != null ? cf.token : "",
+
+                cf.workspace != null ? "--workspace" : "",
+                cf.workspace != null ? cf.workspace : "",
+
+        }).stream().filter(s -> !s.isEmpty()).collect(Collectors.toList());
+
+        return CommandExecutor.executeCommand(command);
     }
 }
