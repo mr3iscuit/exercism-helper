d0d589323a892cc40ae9c27d0ba7ad3d1f34268c
mr3iscuit <eyvaz.bayramov2018@gmail.com>
2024-10-17 15:59:25 +0400

    Implement in app config option

---
+++ src/main/java/org/exidehelper/commands/HelperConfigCommand.java
@@ -0,0 +1,46 @@
+package org.exidehelper.commands;
+
+import org.exidehelper.appConfig.IConfigService;
+import org.exidehelper.exceptions.NoExercismTrackException;
+import picocli.CommandLine;
+
+import java.io.IOException;
+
+@CommandLine.Command(name = "editorconfig", mixinStandardHelpOptions = true, description = "configure exercism helper")
+public class HelperConfigCommand implements Runnable {
+
+    private final IConfigService configService;
+
+    @CommandLine.Option(names = {"--track"}, description = "exercism track ID")
+    private String track;
+
+    @CommandLine.Option(names = {"--editor-path"}, description = "Editor executable path")
+    private String editorPath;
+
+    @CommandLine.Option(names = {"--on-download"}, description = "Open IDE by default on donwload")
+    private Boolean onDownload;
+
+    public HelperConfigCommand(IConfigService configService) {
+        this.configService = configService;
+    }
+
+    @Override
+    public void run() {
+        if (track != null && editorPath != null) {
+            try {
+                configService.setEditorPath(track, editorPath);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        if (onDownload != null) {
+            try {
+                configService.editorOnDownload(onDownload);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+    }
+}
