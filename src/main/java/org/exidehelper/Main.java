3dbc7c14c58678036a53c38f88d6eb6c5d26a665
mr3iscuit <eyvaz.bayramov2018@gmail.com>
2024-10-17 15:55:33 +0400

    Add in app configuration option.

--- src/main/java/org/exidehelper/Main.java
+++ src/main/java/org/exidehelper/Main.java
@@ -1,8 +1,7 @@
 package org.exidehelper;
 import org.exidehelper.appConfig.ConfigService;
-import org.exidehelper.commands.ConfigCommand;
-import org.exidehelper.commands.DownloadCommand;
-import org.exidehelper.commands.WorkspaceCommand;
+import org.exidehelper.appConfig.IConfigService;
+import org.exidehelper.commands.*;
 import org.exidehelper.exercismWrapperService.*;
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
@@ -10,25 +9,29 @@ import picocli.CommandLine.Command;
 
 @Command()
 public class Main implements Runnable {
+
     @Override
     public void run() {
 
     }
     public static void main(String[] args) {
 
-        ConfigService configService = new ConfigService();
+        IConfigService configService = new ConfigService();
         IExercismAPIWrapperService exercismAPIWrapperService = new ExercismAPIWrapperService(configService);
 
         ConfigCommand configCommand = new ConfigCommand(exercismAPIWrapperService);
         DownloadCommand downloadcommand = new DownloadCommand(exercismAPIWrapperService);
 
         WorkspaceCommand workspaceCommand = new WorkspaceCommand(exercismAPIWrapperService);
+        HelperConfigCommand helperConfigCommand = new HelperConfigCommand(configService);
 
         int exitCode = new CommandLine(new Main())
                 .addSubcommand("workspace", workspaceCommand)
                 .addSubcommand("configure", configCommand)
                 .addSubcommand("download", downloadcommand)
+                .addSubcommand("helper-config", helperConfigCommand)
                 .execute(args);
+
         System.exit(exitCode);
     }
 }
